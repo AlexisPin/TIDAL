2852a669c48e2bdf716c48811b33136201768231
Alexis <pinceminalexis@gmail.com>
2022-02-03 22:15:31 +0100

    smarty and little router

--- index.php
+++ index.php
@@ -1,25 +1,34 @@
 <?php
 
+   try {
+      $dbh = new PDO('sqlite:example.db');
+   } catch (PDOException $e) {
+      echo $e->getCode() . ' ' . $e->getMessage();
+   }
+   
+   $sql = "SELECT * FROM Patho WHERE myField='value';";
+   $dbh->query( $sql );
+
    require 'elements/header.php';
 
-    define('MAIN_PATH', getcwd());
-    require(MAIN_PATH . '/inc/smarty-4.0.4/libs/Smarty.class.php');
-    $Smarty = new Smarty();
-    $Smarty->setTemplateDir(MAIN_PATH . '/template');
-    $ConnectionPage = "pages/home.php";
-    $SearchPage = "pages/search.php";
-    $BiblioPage = "pages/bibliography.php";
+   define('MAIN_PATH', getcwd());
+   require(MAIN_PATH . '/inc/smarty-4.0.4/libs/Smarty.class.php');
+   $Smarty = new Smarty();
+   $Smarty->setTemplateDir(MAIN_PATH . '/template');
+   $ConnectionPage = "pages/home.php";
+   $SearchPage = "pages/search.php";
+   $BiblioPage = "pages/bibliography.php";
 
-    $Smarty->assign('ConnectionPage',$ConnectionPage);
-    $Smarty->assign('SearchPage',$SearchPage);
-    $Smarty->assign('BiblioPage',$BiblioPage);
+   $Smarty->assign('ConnectionPage',$ConnectionPage);
+   $Smarty->assign('SearchPage',$SearchPage);
+   $Smarty->assign('BiblioPage',$BiblioPage);
 
    $uri = $_SERVER['REQUEST_URI'];
    require 'elements/navbar.php';
    if ($uri === '/filter') {
-      require 'template/filter.php';
+   require 'template/filter.php';
    } elseif ($uri === '/connexion') {
-      require 'template/home.php';
+   require 'template/home.php';
    }
 
    require 'elements/footer.php';
