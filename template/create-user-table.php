ae524d417aac4561b1ac892dc461007dd2d84940
Leo Gouchon <leo.gouchon@gmail.com>
2022-02-11 08:45:29 +0100

    ajout userId column in user table

--- template/create-user-table.php
+++ template/create-user-table.php
@@ -1,6 +1,7 @@
 <?php
 
     $sql = "CREATE TABLE public.users(
+      userId SMALLINT identity(1,1) PRIMARY KEY,
       username VARCHAR(50) NOT NULL,
       email VARCHAR(50) NOT NULL,
       password VARCHAR(100) NOT NULL,
@@ -10,5 +11,4 @@
       $dbh->exec($sql);
 
       echo "<script>console.log(\"Table bien crée ! \")</script>";
-
 ?>
