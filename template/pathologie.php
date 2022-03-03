a3073cea7e575c5f48ac38172ba7b9f13e94ef9c
Alexis <pinceminalexis@gmail.com>
2022-03-03 21:10:52 +0100

    if id unknown

--- template/pathologie.php
+++ template/pathologie.php
@@ -5,19 +5,24 @@ $pathologie = new Pathologie($dbh);
 
 $pathologie->id = intval($_GET["id"]);
 $result = $pathologie->read_single();
-$symptomes = explode("-", $result['symptome']);
 
+if ($result) {
+    $symptomes = explode("-", $result['symptome']);
 ?>
-<div class="result" id='result_rch_avance'>
-    <div class="patho">
-        <h4><?= $result['pathologie']; ?></h4>
-        <p>Pathologie : <?= $result['meridien']; ?></p>
-        <div class="symptome-container">
-            <h4>Liste des symptomes : </h4>
-            <?php foreach ($symptomes as $symptome) {
-            ?><p><?= $symptome; ?></p>
-            <?php
-            } ?>
+    <div class="result" id='result_rch_avance'>
+        <div class="patho">
+            <h4><?= $result['pathologie']; ?></h4>
+            <p>Pathologie : <?= $result['meridien']; ?></p>
+            <div class="symptome-container">
+                <h4>Liste des symptomes : </h4>
+                <?php foreach ($symptomes as $symptome) {
+                ?><p><?= $symptome; ?></p>
+                <?php
+                } ?>
+            </div>
         </div>
     </div>
-</div>
+<?php
+} else {
+    header("Location: / ");
+}
